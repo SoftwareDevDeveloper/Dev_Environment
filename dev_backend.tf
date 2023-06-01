aa67531f6fac3088a95b6c0c183c3a992e365382
Rufus Ajiboye <ruufman@gmail.com>
2023-06-01 15:53:10 +0100

    New codes added to the Dev repo

---
+++ dev_backend.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  backend "s3" {
+    bucket = "devops-expert-terraform-state-files"
+    key    = "development/terraform.tfstate"
+    region = "eu-west-1"
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "ec2_ami" {
+  description = "This is a variable to manage ec2 AMI type"
+  type        = string
+  default     = "ami-04f7efe62f419d9f5"
+}
+
+variable "ec2_instance_type" {
+  description = "This is a variable to manage ec2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ec2_key_name" {
+  description = "This is a variable to manage ec2 key name"
+  type        = string
+  default     = "test100"
+}
+
+variable "default_tags" {
+  description = "This variable is declared here to manage ec2 tags"
+  type        = map(any)
+  default = {
+    "Name" = "Development Environment"
+  }
+}
+
